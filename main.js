7bfecf7c2396f0d030a29b2387b2f6169396dc3e
Muhammad Usman <muhammadusman09858@gmail.com>
2025-09-08 04:40:19 -0700

    footer complete hogya

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.getElementById("Nav").innerHTML =  `
         <nav class="navbar navbar-expand-lg ">
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.html">
-                    <img src="assets/logo.jpg" alt="" class="image">
+                    <img src="assets/logo.png" alt="" class="image">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -46,3 +46,126 @@ document.getElementById("Nav").innerHTML =  `
         </nav>
     `
   ;
+
+  document.getElementById("footer").innerHTML=`
+  <footer class="text-center text-lg-start  text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Vinger food
+          </h6>
+          <p>
+           Finger foods chhote aur asaani se khaye jaane wale snacks hote hain, jo parties aur gatherings mein perfect hote hain. Yeh tasty aur convenient hote hain, jise haath se bina plate ke bhi enjoy kiya ja sakta hai. Samosa, pakora, spring rolls jaise options sabko pasand aate hain!
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Use full links
+          </h6>
+          <p>
+            <a href="index.html" class="text-reset">home</a>
+          </p>
+          <p>
+            <a href="about us.html" class="text-reset">about us</a>
+          </p>
+          <p>
+            <a href="gallery.html" class="text-reset">Gallery</a>
+          </p>
+          <p>
+            <a href="awards.html" class="text-reset">Awards</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="catering.html" class="text-reset">catering</a>
+          </p>
+          <p>
+            <a href="recipe of the month.html" class="text-reset">recipe of month</a>
+          </p>
+          <p>
+            <a href="contuct us.html" class="text-reset">contuct us</a>
+          </p>
+          <p>
+            <a href="feedback.html" class="text-reset">feedback</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i>aptech metro star gate</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            muhammadusman09858@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i>  03152724554</p>
+          <p><i class="fas fa-print me-3"></i>  o3276524345</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2025 muhammad usman:
+    <a class="text-reset fw-bold" href="vinger food">vinger f ood .com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+  
+  
+  
+  `
